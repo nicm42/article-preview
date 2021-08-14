3868e4db34971a67ff079e49523e9828e4b9663b
Nic <42087659+nicm42@users.noreply.github.com>
2021-08-14 12:53:12 +0100

    Added Cypress test for text direction

--- cypress/integration/page.test.ts
+++ cypress/integration/page.test.ts
@@ -1,9 +1,11 @@
 /// <reference types="Cypress" />
 
 describe('Page tests', () => {
-  it('Tests the page loads and the share button works', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:8000');
+  });
 
+  it('Tests the page loads and the share button works', () => {
     // Check share pop-up appears when click on button
     cy.get('button').first().click();
     cy.get('.content__share').should('be.visible');
@@ -11,5 +13,16 @@ describe('Page tests', () => {
     // And disappears when you click on it again
     cy.get('button').first().click();
     cy.get('.content__share').should('not.be.visible');
+  });
+
+  it.only('Tests LTR and RTL radio buttons', () => {
+    // Check direction is RTL when click on that radio button
+    // Need the force: true because the radio button has appearance none
+    cy.get('#rtl').check({ force: true });
+    cy.document().should('have.attr', 'dir', 'rtl');
+
+    // Check direction is LTr when click on that radio button
+    cy.get('#ltr').check({ force: true });
+    cy.document().should('have.attr', 'dir', 'ltr');
   });
 });
